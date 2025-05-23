d2c8bc17bba4ceba56e1eeedfeb93dba2dd4ce8d
mateus <mateusdev23@gmail.com>
2025-05-22 21:22:11 -0300

    Criando e conectando ao Node.js ao MongoDB

---
+++ src/database/mongo.js
@@ -0,0 +1,22 @@
+import { text } from 'express';
+import { MongoClient } from 'mongodb';
+
+export const Mongo = {
+    async connect({mongoConnectionString, mongoDbName}) {
+        try {
+            const client = new MongoClient(mongoConnectionString)
+
+            await client.connect()
+            const db = client.db(mongoDbName)
+
+            this.client = client
+            this.db = db
+
+            return 'Conectado ao mongo'
+
+        } catch(error) {
+            return { text: 'Erro durante a conexão do mongo', error}
+        }
+        
+    }
+}
