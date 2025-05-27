098e1b698cd8d3b1d86b4458bafbd9818101ae07
mateus <mateusdev23@gmail.com>
2025-05-26 22:27:05 -0300

    Iniciando a iteração com o banco de dados e resolvendo falha com o bd

---
+++ src/controllers/users.js
@@ -0,0 +1,19 @@
+import UsersDataAccess from "../dataAccess/users.js";
+import { ok, serverError } from '../helpers/httpResponse.js'
+
+export default class UsersControllers {
+    constructor() {
+        this.dataAccess = new UsersDataAccess()
+    }
+
+    async getUsers() {
+        try {
+            const users = await this.dataAccess.getUsers()
+            // console.log(users)
+
+            return ok(users)
+        } catch (error) {
+            return serverError(error)
+        }
+    }
+}
