4354151515202c9fa6cd322c3ff45361a7910dad
mateus <mateusdev23@gmail.com>
2025-08-07 22:30:01 -0300

    Iniciando o Dashboard

--- dentalestoque-frontend/src/pages/Dashboard/index.jsx
+++ dentalestoque-frontend/src/pages/Dashboard/index.jsx
@@ -5,7 +5,66 @@ export default function Dashboard() {
     return (
         <div className="main-content">
             <div className="page-title">
-                <Title name={"Dashboard"}/>
+                <Title className="title" name={"Dashboard"}/>
+                <div className="actions-buttons">
+                    <button className="btn btn-primary">
+                        <i className="fas fa-download"></i>
+                        Relatório
+                    </button>
+                </div>
+            </div>
+
+            <div className="stats-cards">
+                <div className="stat-card">
+                    <div className="card-header">
+                        <div>
+                            <div className="card-value">54.554</div>
+                            <div className="card-label">Total Produtos em Estoque</div>
+                        </div>
+                        <div className="card-icon">
+                            <i className="fas fa-box"></i>
+                        </div>                       
+                    </div>
+                </div>
+
+                <div className="stat-card">
+                    <div className="card-header">
+                        <div>
+                            <div className="card-value">12,750</div>
+                            <div className="card-label">Total de Entradas</div>
+                        </div>
+                        <div className="card-icon">
+                            <i className="fas fa-right-to-bracket"></i>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div className="stat-card">
+                    <div className="card-header">
+                        <div>
+                            <div className="card-value">8.524</div>
+                            <div className="card-label">Total de Saidas</div>
+                        </div>
+                        <div className="card-icon">
+                            <i className="fas fa-right-from-bracket"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="stat-card">
+                    <div className="card-header">
+                        <div>
+                            <div className="card-value">15.000</div>
+                            <div className="card-label">Saldo em Estoque</div>
+                        </div>
+                        
+                        <div className="card-icon">
+                            <i className="fas fa-chart-pie"></i>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
